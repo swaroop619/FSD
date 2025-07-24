8dfc8d40b9e8e500b146e49d1671c1ce9fca21be
Chaya Swaroop <52211223+swaroop619@users.noreply.github.com>
2025-07-24 10:01:44 +0530

    Add files via upload

---
+++ node_assignment4/user.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+
+const userSchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    age: { type: Number }
+});
+
+
+module.exports = mongoose.model('User', userSchema);
